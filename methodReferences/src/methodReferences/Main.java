51663e7149a57d5f40b0c725b96c055692319fbb
Sourabh Shinde <shindesourabh3811@gmail.com>
2025-06-02 12:50:09 +0530

    Method References In java

---
+++ methodReferences/src/methodReferences/Main.java
@@ -0,0 +1,35 @@
+package methodReferences;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+	
+	
+	/*
+	 * 
+	 * n Java, method references are a shorthand notation of lambda expressions to call a method.
+	 *  They were introduced in Java 8 and help make code more readable and concise when you're just calling an existing method.
+	 *  syntax ----->ClassName::methodName
+	 *  
+
+	 */
+
+	public static void main(String[] args) {
+//		List.of(1,2,3,4,5).forEach(x->System.out.println(x));
+		
+		//method reference
+		List.of(1,2,3,4,5).forEach(System.out::println);
+
+		List<String> str = Arrays.asList("sam","ram");
+//	    str.sort((s1,s2)->s1.equalsIgnoreCase(s2));
+		
+		//Using Method Reference
+	    str.sort(String::compareToIgnoreCase);
+	    System.out.println(str);  
+
+		
+	}
+}
+
